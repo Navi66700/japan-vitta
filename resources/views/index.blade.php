3c087a030551b1c2bfb9231f0f22d0c7763456e0
Nadun Wijerathna <nadunadithya123@gmail.com>
2022-12-21 12:26:25 +0530

    Edit Front Stuff

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@
   "></div>
     <div class="video-overlay header-text">
         <div class="caption">
-            <h6>Go For Your Japan Dream</h6>
+            <h6>Japan is not a Dream Anymore </h6>
             <h2><em>Japan</em> Vitta</h2>
 
         </div>
@@ -72,60 +72,60 @@
 
 
 
-    <div class="row row-cols-1 row-cols-md why-us" data-section="section2">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="section-heading">
-                    <h2>Why choose Grad School?</h2>
-                </div>
-            </div>
-            <div class="col-md-12">
-                <div id='tabs'>
-                    <ul>
-                        <li><a href='#tabs-1'>Best Education</a></li>
-                        <li><a href='#tabs-2'>Top Management</a></li>
-                        <li><a href='#tabs-3'>Quality Meeting</a></li>
-                    </ul>
-                    <section class='tabs-content'>
-                        <article id='tabs-1'>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <img src="{{asset('homepage/assets/images/choose-us-image-01.png')}}" alt="">
-                                </div>
-                                <div class="col-md-6">
-                                    <h4>Best Education</h4>
-                                    <p>Grad School is free educational HTML template with Bootstrap 4.5.2 CSS layout. Feel free to use it for educational or commercial purposes. You may want to make <a href="https://paypal.me/templatemo" target="_parent" rel="sponsored">a little donation</a> to TemplateMo. Please tell your friends about us. Thank you.</p>
-                                </div>
-                            </div>
-                        </article>
-                        <article id='tabs-2'>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <img src="{{asset('homepage/assets/images/choose-us-image-02.png')}}" alt="">
-                                </div>
-                                <div class="col-md-6">
-                                    <h4>Top Level</h4>
-                                    <p>You can modify this HTML layout by editing contents and adding more pages as you needed. Since this template has options to add dropdown menus, you can put many HTML pages.</p>
-                                    <p>Suspendisse tincidunt, magna ut finibus rutrum, libero dolor euismod odio, nec interdum quam felis non ante.</p>
-                                </div>
-                            </div>
-                        </article>
-                        <article id='tabs-3'>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <img src="{{asset('homepage/assets/images/choose-us-image-03.png')}}" alt="">
-                                </div>
-                                <div class="col-md-6">
-                                    <h4>Quality Meeting</h4>
-                                    <p>You are NOT allowed to redistribute this template ZIP file on any template collection website. However, you can use this template to convert into a specific theme for any kind of CMS platform such as WordPress. For more information, you shall <a rel="nofollow" href="https://templatemo.com/contact" target="_parent">contact TemplateMo</a> now.</p>
-                                </div>
-                            </div>
-                        </article>
-                    </section>
-                </div>
-            </div>
-        </div>
-    </div>
+{{--    <div class="row row-cols-1 row-cols-md why-us" data-section="section2">--}}
+{{--        <div class="row">--}}
+{{--            <div class="col-md-12">--}}
+{{--                <div class="section-heading">--}}
+{{--                    <h2>Why choose Grad School?</h2>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <div class="col-md-12">--}}
+{{--                <div id='tabs'>--}}
+{{--                    <ul>--}}
+{{--                        <li><a href='#tabs-1'>Best Education</a></li>--}}
+{{--                        <li><a href='#tabs-2'>Top Management</a></li>--}}
+{{--                        <li><a href='#tabs-3'>Quality Meeting</a></li>--}}
+{{--                    </ul>--}}
+{{--                    <section class='tabs-content'>--}}
+{{--                        <article id='tabs-1'>--}}
+{{--                            <div class="row">--}}
+{{--                                <div class="col-md-6">--}}
+{{--                                    <img src="{{asset('homepage/assets/images/choose-us-image-01.png')}}" alt="">--}}
+{{--                                </div>--}}
+{{--                                <div class="col-md-6">--}}
+{{--                                    <h4>Best Education</h4>--}}
+{{--                                    <p>Grad School is free educational HTML template with Bootstrap 4.5.2 CSS layout. Feel free to use it for educational or commercial purposes. You may want to make <a href="https://paypal.me/templatemo" target="_parent" rel="sponsored">a little donation</a> to TemplateMo. Please tell your friends about us. Thank you.</p>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </article>--}}
+{{--                        <article id='tabs-2'>--}}
+{{--                            <div class="row">--}}
+{{--                                <div class="col-md-6">--}}
+{{--                                    <img src="{{asset('homepage/assets/images/choose-us-image-02.png')}}" alt="">--}}
+{{--                                </div>--}}
+{{--                                <div class="col-md-6">--}}
+{{--                                    <h4>Top Level</h4>--}}
+{{--                                    <p>You can modify this HTML layout by editing contents and adding more pages as you needed. Since this template has options to add dropdown menus, you can put many HTML pages.</p>--}}
+{{--                                    <p>Suspendisse tincidunt, magna ut finibus rutrum, libero dolor euismod odio, nec interdum quam felis non ante.</p>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </article>--}}
+{{--                        <article id='tabs-3'>--}}
+{{--                            <div class="row">--}}
+{{--                                <div class="col-md-6">--}}
+{{--                                    <img src="{{asset('homepage/assets/images/choose-us-image-03.png')}}" alt="">--}}
+{{--                                </div>--}}
+{{--                                <div class="col-md-6">--}}
+{{--                                    <h4>Quality Meeting</h4>--}}
+{{--                                    <p>You are NOT allowed to redistribute this template ZIP file on any template collection website. However, you can use this template to convert into a specific theme for any kind of CMS platform such as WordPress. For more information, you shall <a rel="nofollow" href="https://templatemo.com/contact" target="_parent">contact TemplateMo</a> now.</p>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                        </article>--}}
+{{--                    </section>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 
 
 
@@ -400,7 +400,60 @@
             </div>
         </div>
     </div>
-
+    <div class="row row-cols-1 row-cols-md why-us" data-section="section2">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-heading">
+                    <h2>Why choose Grad School?</h2>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div id='tabs'>
+                    <ul>
+                        <li><a href='#tabs-1'>Best Education</a></li>
+                        <li><a href='#tabs-2'>Top Management</a></li>
+                        <li><a href='#tabs-3'>Quality Meeting</a></li>
+                    </ul>
+                    <section class='tabs-content'>
+                        <article id='tabs-1'>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <img src="{{asset('homepage/assets/images/choose-us-image-01.png')}}" alt="">
+                                </div>
+                                <div class="col-md-6">
+                                    <h4>Best Education</h4>
+                                    <p>Grad School is free educational HTML template with Bootstrap 4.5.2 CSS layout. Feel free to use it for educational or commercial purposes. You may want to make <a href="https://paypal.me/templatemo" target="_parent" rel="sponsored">a little donation</a> to TemplateMo. Please tell your friends about us. Thank you.</p>
+                                </div>
+                            </div>
+                        </article>
+                        <article id='tabs-2'>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <img src="{{asset('homepage/assets/images/choose-us-image-02.png')}}" alt="">
+                                </div>
+                                <div class="col-md-6">
+                                    <h4>Top Level</h4>
+                                    <p>You can modify this HTML layout by editing contents and adding more pages as you needed. Since this template has options to add dropdown menus, you can put many HTML pages.</p>
+                                    <p>Suspendisse tincidunt, magna ut finibus rutrum, libero dolor euismod odio, nec interdum quam felis non ante.</p>
+                                </div>
+                            </div>
+                        </article>
+                        <article id='tabs-3'>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <img src="{{asset('homepage/assets/images/choose-us-image-03.png')}}" alt="">
+                                </div>
+                                <div class="col-md-6">
+                                    <h4>Quality Meeting</h4>
+                                    <p>You are NOT allowed to redistribute this template ZIP file on any template collection website. However, you can use this template to convert into a specific theme for any kind of CMS platform such as WordPress. For more information, you shall <a rel="nofollow" href="https://templatemo.com/contact" target="_parent">contact TemplateMo</a> now.</p>
+                                </div>
+                            </div>
+                        </article>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <div class="row row-cols-1 row-cols-md video" data-section="section5">
     <center>
--- resources/views/layout/masters.blade.php
+++ resources/views/layout/masters.blade.php
@@ -79,7 +79,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <p><i class="fa fa-copyright"></i> Copyright 2020 by Grad School
+                <p><i class="fa fa-copyright"></i> Copyright 2023 Japan Vitta
 
                     | Design: <a href="https://templatemo.com" rel="sponsored" target="_parent">TemplateMo</a></p>
 
