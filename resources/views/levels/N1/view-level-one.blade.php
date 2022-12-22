d8c33d6d94ae8e90df8301743ca2254a3faca480
Navi66700 <68125951+Navi66700@users.noreply.github.com>
2022-12-22 23:35:03 +0530

    View Level Changes

--- resources/views/levels/N1/view-level-one.blade.php
+++ resources/views/levels/N1/view-level-one.blade.php
@@ -2,30 +2,33 @@
 @section('title', 'Home')
 @section('content')
 
-    <div class="container-fluid">
+ <div class="container-fluid">
 <div class="row row-cols-1 row-cols-md video " data-section="section5">
+    <input type="hidden" id="lessonones_id" name="lessonones_id" value="{{ $lessononeDetails->id }}">
     <div class="row">
-        <input type="hidden" id="lessonones_id" name="lessonones_id" value="{{ $lessononeDetails->id }}">
-        <center>
-            <h1 style="color: white; font-weight: bold">{{ $lessononeDetails->lesson_title }}</h1>
-        </center>
-        <br><br><br><br><br><br><br>
+        <div class="col-md-12">
+            <center>
+                <h1 style="color: white; font-weight: bold;">{{ $lessononeDetails->lesson_title }}</h1>
+            </center>
+        </div>
+    </div>
+    <br><br><br>
+    <div class="row">
         <div class="col-md-6 text-center">
             <div class="left-content">
                 <iframe id="preview_pdf" name="pdf_file" class="img-fluid rounded" style="width: 300px; height: 500px;" src="{{asset('storage/lessons-pdf/'.$lessononeDetails->pdf_file)}}"></iframe>
                 <div class="main-button"><a rel="nofollow" href="https://fb.com/templatemo" target="_parent">Download Lesson</a></div>
             </div>
         </div>
-
         <div class="col-md-6">
-            <h4 style="color: white; font-weight: bold">{{ $lessononeDetails->description }}</h4>
+            <h6 style="color: white; font-weight: bold">{{ $lessononeDetails->description }}</h6>
         </div>
     </div>
-    <div class="row">
-        <div class="col-md-12">
+        <div class="col">
             <div class="video-caption">
                 <!-- <h4>Learn About Japan</h4> -->
             </div>
+            <br><br><br><br>
             <article class="video-item">
                 <figure>
                     <div class="video-container">
@@ -37,7 +40,7 @@
                     </div>
                 </figure>
             </article>
-        </div>
+
     </div>
 </div>
     </div>
