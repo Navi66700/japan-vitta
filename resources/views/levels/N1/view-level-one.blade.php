c2ce17c7d8ea2301bd10a5cc3417dec42e5062ee
Navi66700 <68125951+Navi66700@users.noreply.github.com>
2022-12-22 23:13:06 +0530

    Home page changes

--- resources/views/levels/N1/view-level-one.blade.php
+++ resources/views/levels/N1/view-level-one.blade.php
@@ -2,22 +2,27 @@
 @section('title', 'Home')
 @section('content')
 
-
+    <div class="container-fluid">
 <div class="row row-cols-1 row-cols-md video " data-section="section5">
     <div class="row">
         <input type="hidden" id="lessonones_id" name="lessonones_id" value="{{ $lessononeDetails->id }}">
         <center>
             <h1 style="color: white; font-weight: bold">{{ $lessononeDetails->lesson_title }}</h1>
         </center>
+        <br><br><br><br><br><br><br>
         <div class="col-md-6 text-center">
             <div class="left-content">
-                <h4>{{ $lessononeDetails->description }}</h4>
                 <iframe id="preview_pdf" name="pdf_file" class="img-fluid rounded" style="width: 300px; height: 500px;" src="{{asset('storage/lessons-pdf/'.$lessononeDetails->pdf_file)}}"></iframe>
                 <div class="main-button"><a rel="nofollow" href="https://fb.com/templatemo" target="_parent">Download Lesson</a></div>
             </div>
         </div>
-        
+
         <div class="col-md-6">
+            <h4 style="color: white; font-weight: bold">{{ $lessononeDetails->description }}</h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
             <div class="video-caption">
                 <!-- <h4>Learn About Japan</h4> -->
             </div>
@@ -35,4 +40,5 @@
         </div>
     </div>
 </div>
+    </div>
 @endsection
