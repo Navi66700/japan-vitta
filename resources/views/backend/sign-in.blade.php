cc161bbbe5a82a2782c3503da273b2c6b2e0a76f
Navi66700 <68125951+Navi66700@users.noreply.github.com>
2023-03-23 15:45:08 +0530

    UI Changes

--- resources/views/backend/sign-in.blade.php
+++ resources/views/backend/sign-in.blade.php
@@ -32,18 +32,18 @@
 
 </head>
 
-<body>
-<main style="background-image: url('https://mdbcdn.b-cdn.net/img/Photos/Others/images/77.webp');">
+<body style="background-image: url('{{asset('backend/assets/img/japan-b-i.jpeg')}}'); background-size: cover;">
+<main>
     <div class="container">
         <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
                         <div class="d-flex justify-content-center py-4">
-                            <a href="index.html" class="logo d-flex align-items-center w-auto">
+                            {{-- <a href="index.html" class="logo d-flex align-items-center w-auto">
                                 <img src="{{asset('homepage/assets/images/jp-logo.png')}}" alt="">
                                 <span >Japan Vitta</span>
-                            </a>
+                            </a> --}}
                         </div><!-- End Logo -->
 
                         <div class="card mb-3">
@@ -57,6 +57,10 @@
                                     </div>
                                 @endif
                                 <div class="pt-4 pb-2">
+                                    <center>
+                                    <img src="{{asset('homepage/assets/images/jp-logo.png')}}" alt="" style="height:100px; width:100px;"><br>
+                                    <h3>Japan Vitta</h3>
+                                </center>
                                     <h5 class="card-title text-center pb-0 fs-4">Login</h5>
                                 </div>
 
